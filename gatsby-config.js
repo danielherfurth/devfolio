3d86503dc385f21ac93edb60f22bc6138b998df6
danielherfurth <danielherfurth@gmail.com>
2022-04-29 20:38:03 -0400

    title_about

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
                 description:
                     'A script to automate batch conversion of .xlsx files to .pdf files.',
                 link: 'https://github.com/danielherfurth/Excel-to-PDF/blob/main/xl_to_pdf.py',
-            },
+            }
         ],
         // Optional: List your experience, they must have `name` and `description`. `link` is optional.
         experience: [
