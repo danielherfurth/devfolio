3aa5c96e4557adef08c8191445ce5af2416d4505
danielherfurth <danielherfurth@gmail.com>
2022-05-03 18:02:31 -0400

    Revert "title_about"
    This reverts commit 57929ff0

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
         // Site URL for when it goes live
-        siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+        siteUrl: `https://heartfelt-pasca-ec9f2a.netlify.app/`,
         // Your Name
         name: 'Daniel Herfurth',
         // Main Site Title
