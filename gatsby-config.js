d413be996e64f08d7ba234300c26d5ab325bafec
danielherfurth <danielherfurth@gmail.com>
2022-05-03 17:41:39 -0400

    title_about

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,19 +68,15 @@ module.exports = {
         // Optional: List your experience, they must have `name` and `description`. `link` is optional.
         experience: [
             {
-                name: 'Acme Corp',
-                description: 'Full-Stack Developer, February 2020 - Present',
-                link: 'https://github.com/RyanFitzgerald/devfolio',
+                name: 'Graduate Research Assistant, Stewart Lab',
+                description: 'Small-molecule synthesis and product Analysis. ' +
+                    'May2021 - Present',
+                // link: 'https://github.com/RyanFitzgerald/devfolio',
             },
             {
-                name: 'Globex Corp',
-                description: 'Full-Stack Developer, December 2017 - February 2020',
-                link: 'https://github.com/RyanFitzgerald/devfolio',
-            },
-            {
-                name: 'Hooli',
-                description: 'Full-Stack Developer, May 2015 - December 2017',
-                link: 'https://github.com/RyanFitzgerald/devfolio',
+                name: 'ACM Chemistries',
+                description: 'R&D of product from concept to market. Method development and validation for QC of product.' +
+                    'April 2019 - April 2021'
             },
         ],
         // Optional: List your skills, they must have `name` and `description`.
