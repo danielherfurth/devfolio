9346e97b299b5a49e39e94cf346b19a00a16b36f
danielherfurth <danielherfurth@gmail.com>
2022-05-03 18:29:59 -0400

    tr

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,13 +71,13 @@ module.exports = {
                 name: 'Graduate Research Assistant, Stewart Lab',
                 description: 'Small-molecule synthesis and product Analysis. ' +
                     'May2021 - Present',
-                // link: 'https://github.com/RyanFitzgerald/devfolio',
+                link: 'https://github.com/RyanFitzgerald/devfolio',
             },
             {
                 name: 'ACM Chemistries',
                 description: 'R&D of product from concept to market. Method development and validation for QC of product.' +
                     'April 2019 - April 2021',
-                // link: 'https://github.com/RyanFitzgerald/devfolio',
+                link: 'https://github.com/RyanFitzgerald/devfolio',
             },
             //
             // {
