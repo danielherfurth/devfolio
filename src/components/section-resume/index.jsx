b756a2ab9e6bd5a87504e89beee18fa7e524b102
danielherfurth <danielherfurth@gmail.com>
2022-07-28 17:20:12 -0400

    tr

--- src/components/section-resume/index.jsx
+++ src/components/section-resume/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 
 import Section from '../section';
-
-
-const classes = {
-    title: 'pb-6 md:w-full md:max-w-150 md:p-0 hover:text-underline'
-};
-
+import SummaryItem from "../summary-item";
 
 const SectionResume = ({resume}) => {
     return (
-
-            <Section title="Resume">
-                <div className={classes.title}>
+        <Section title="Resume">
+            {resume.map((res) => (
+                <SummaryItem
+                key={res.name}
+                name={res.name}
+                description={res.description()}
+                </>
+            ))}
+            <div>
                 <a
                     href="https://danielherfurth.github.io/DanielHerfurthResume_data.pdf"
                 >
                     Full Resume
                 </a></div>
-            </Section>
+        </Section>
 
     );
 };
