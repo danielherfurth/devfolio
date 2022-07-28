217b3d0ef5adfeb2f59a6f07ffe09750a845c9c9
danielherfurth <danielherfurth@gmail.com>
2022-07-28 15:34:02 -0400

    tr

--- src/components/section-resume/index.jsx
+++ src/components/section-resume/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import classes from '../header';
 import Section from '../section';
 
 const SectionResume = ({resume}) => {
     return (
         <Section title="Resume">
-            <div className="mb-6">
+            <div className={classes.name}>
                 <p>
                     <a
                         href="https://danielherfurth.github.io/DanielHerfurthResume_data.pdf"
