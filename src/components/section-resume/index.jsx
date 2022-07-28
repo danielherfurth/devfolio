a3ad029db315d614db5f916fef8aeafc497196d4
danielherfurth <danielherfurth@gmail.com>
2022-07-28 15:17:58 -0400

    tr

--- src/components/section-resume/index.jsx
+++ src/components/section-resume/index.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 import Section from '../section';
 
-const SectionResume = ({ resume }) => {
-  return (
-    <Section title="Resume">
-      <div className="mb-6">
-        <p>
-            <a
-                href="https://danielherfurth.github.io/DanielHerfurthResume_data.pdf"
-            >
-            "Full Resume"
-            </a>
-        </p>
-      </div>
-    </Section>
-  );
+const SectionResume = ({resume}) => {
+    return (
+        <Section title="Resume">
+            <div className="mb-6">
+                <p>
+                    <a
+                        href="https://danielherfurth.github.io/DanielHerfurthResume_data.pdf"
+                    >
+                        <b>Full Resume</b>
+                    </a>
+                </p>
+            </div>
+        </Section>
+    );
 };
 
 export default SectionResume;
