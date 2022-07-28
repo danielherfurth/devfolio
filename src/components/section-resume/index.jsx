101bbb80e4d580a6187bee1acdc340133a0fb17d
danielherfurth <danielherfurth@gmail.com>
2022-07-28 17:29:13 -0400

    tr

--- src/components/section-resume/index.jsx
+++ src/components/section-resume/index.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 import Section from '../section';
 import SummaryItem from "../summary-item";
 
-const SectionResume = ({resume}) => {
-    return (
-        <Section title="Resume">
-                <a
-                    href="https://danielherfurth.github.io/DanielHerfurthResume_data.pdf"
-                >
-                    Full Resume
-                </a>
-        </Section>
+const classes = {
+    wrapper: 'block pt-12 md:flex',
+    title: 'pb-6 md:w-full md:max-w-150 md:p-0',
+    heading:
+        'font-xs font-light tracking-widest text-sm text-gray-600 leading-normal uppercase',
+    content: 'flex-none text-lg text-gray-600 font-light md:flex-1 md:pl-20',
+};
 
+const SectionResume = ({title, children}) => {
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.title}>
+                <h2 className={classes.heading}>{title}</h2>
+            </div>
+            <div className={classes.content}>{children}</div>
+        </div>
     );
 };
 
